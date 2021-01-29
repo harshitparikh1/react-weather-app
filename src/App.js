cf81f30ccd92c64cd7077ccb4e99e2b35f8b010e
Harshit Parikh <harshitparikh12@gmail.com>
2021-01-29 14:21:39 +0530

    Fetched api and added weather call for each city.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ const api = {
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -18,30 +32,41 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
 
-
   return (
     <div className="app">
         <main>
           <div className="search-box">
-              <input
-                type="text"
-                className="search-bar"
-                placeholder="Search..."
-              />
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search..."
+              onChange={e => setQuery(e.target.value)}
+              value = {query}
+              onKeyPress={search}
+            />
           </div>
 
+        { (typeof weather.main != "undefined") ? (
+
+          <div>
+            
           <div className="location-box">
-            <div className="location">New York City</div>
+            <div className="location">{weather.name}, {weather.sys.country} </div>
             <div className="date"> {dateBuilder(new Date())}  </div>
           </div>
 
           <div className="weather-box">
             <div className="temp">
-              15° Celsius
+            {Math.round(weather.main.temp)}°C
             </div>
-            <div className="weather"> Sunny </div>
+            <div className="weather"> {weather.weather[0].main} </div>
 
           </div>
+          
+          </div>
+          )
+          : ('')
+        }
 
         </main>
     </div>
